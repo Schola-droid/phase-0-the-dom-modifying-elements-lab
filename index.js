2b45a72ac942bc23dd06be8717d8c5ce9ca5c2dc
Scholastica Nzomo <scholambula1@gmail.com>
2023-03-24 08:55:20 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+//let main=document.getElementById ('main')
+//main.remove()
+document.getElementById ('main').remove()
+const newHeader = document.createElement ("h1");
+newHeader.id = ("victory")
+newHeader.textContent = "SCHOLA is the champion"
